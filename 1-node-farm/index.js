dbaa638796ce2789dc215209cad97de0c4d62489
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-10-06 19:28:19 +0200

    added template replacement and serving

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -4,21 +4,65 @@ const url = require('url');
 
 // The synchronous code blocks execution, which in this case is the POINT
 // It also executes only once
+
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  }
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
 
 const server = http.createServer((request, response) => {
   const pathName = request.url;
 
+  // Overview page
   if (pathName === '/overview' || pathName === '/') {
-    response.end(`This is the overview`);
+    response.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cardsHtml = dataObject
+      .map(element => replaceTemplate(tempCard, element))
+      .join('');
+
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+    response.end(output);
+
+    // Product page
   } else if (pathName === '/product') {
     response.end(`This is the product`);
+
+    // API
   } else if (pathName === '/api') {
     response.writeHead(200, {
       'Content-type': 'application/json',
     });
     response.end(data);
+
+    // Not found
   } else {
     response.writeHead(404, {
       'Content-type': 'text/html',
--- notes/Intro to Node and NPM.md
+++ notes/Intro to Node and NPM.md
@@ -293,3 +293,61 @@ We can replace boilerplate with placeholders in order to update programmatically
   <span>Add to shopping card ({%PRICE%}€)</span>
 </a>
 ```
+
+# Filling templates
+
+In order to work with templates we need to load them into memory
+
+### Loading html
+
+```javascript
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+```
+
+### Replacing content in the page
+
+The api endpoint
+
+```javascript
+if (pathName === '/overview' || pathName === '/') {
+  response.writeHead(200, { 'Content-type': 'text/html' });
+
+  const cardsHtml = dataObject
+    .map(element => replaceTemplate(tempCard, element))
+    .join('');
+
+  const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+  response.end(output);
+}
+```
+
+The function
+
+```javascript
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  }
+  return output;
+};
+```
