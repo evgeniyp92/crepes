2f8186d8d6bf424ced82b5264af42ba9b50b4704
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-10-25 15:01:49 +0200

    set up basic get route

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 // Initializing application
 const app = express();
@@ -7,15 +8,30 @@ const PORT = 4000;
 
 // defining a route
 // note that express requests are a bit more detailed than the node http lib
-app.get('/', (request, response) => {
-  response.status(200).json({
-    message: `Hello from the server side!`,
-    app: 'Natours',
-  });
-});
+// app.get('/', (request, response) => {
+//   response.status(200).json({
+//     message: `Hello from the server side!`,
+//     app: 'Natours',
+//   });
+// });
 
-app.post('/', (request, response) => {
-  response.send(`You can post to this endpoint 😳`);
+// app.post('/', (request, response) => {
+//   response.send(`You can post to this endpoint 😳`);
+// });
+
+const tours = JSON.parse(
+  // @ts-ignore
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (request, response) => {
+  response.json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 // Setting up a listen
