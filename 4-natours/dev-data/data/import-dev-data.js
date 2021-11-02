c97fc474803e3d46cc5d7bac7755527171b05aa4
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-11-02 15:24:09 +0100

    wrote helper script to remove and reimport test data

---
+++ 4-natours/dev-data/data/import-dev-data.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DB_CONN_STRING.replace(
+  '<PASSWORD>',
+  process.env.DB_ADM_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((connection) => {
+    console.log(`MongoDB Connected!`);
+  });
+
+// READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT DATA INTO DATABASE
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log(`Data successfully loaded!`);
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM COLLECTION
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log(`Data deleted!`);
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
