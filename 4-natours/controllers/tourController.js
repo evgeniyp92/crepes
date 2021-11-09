885c3280156fbb4ea4ab7e0a77b6ee1155b5424f
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-11-09 10:35:03 +0100

    extracted api features to own class in own file

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.aliasTopTours = (request, response, next) => {
   request.query.limit = '5';
@@ -9,53 +10,14 @@ exports.aliasTopTours = (request, response, next) => {
 
 exports.getAllTours = async (request, response) => {
   try {
-    // 1A. BUILD QUERY
-    const queryObj = { ...request.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((element) => delete queryObj[element]);
+    const features = new APIFeatures(Tour.find(), request.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
-    // 1B. ADVANCED FILTERING
-    let queryString = JSON.stringify(queryObj);
-    queryString = queryString.replace(
-      /\b(gte|gt|lte|lt)\b/g,
-      (matchedTerm) => `$${matchedTerm}`
-    );
+    const allTours = await features.query;
 
-    let query = Tour.find(JSON.parse(queryString));
-
-    // 2. SORTING
-    if (request.query.sort) {
-      const sortBy = request.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3. FIELD LIMITING
-    if (request.query.fields) {
-      // create a string of selected fields and process it
-      const fields = request.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      // exclude the v field
-      query.select('-__v');
-    }
-
-    // 4. PAGINATION
-    // http://localhost:4000/api/v1/tours?page=2&limit=10
-    const page = request.query.page * 1 || 1;
-    const limit = request.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    // 1-10 page 1, 11-20 page 2, 21-30 page 3 etc etc
-    query = query.skip(skip).limit(limit);
-
-    if (request.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist.');
-    }
-
-    // EXECUTE QUERY
-    const allTours = await query;
     // query.sort().select().skip().limit()
 
     // using mongoose to execute the search
