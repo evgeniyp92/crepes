c0cc7db7ad4897e22cc926b9d6f11fd1ea59beea
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-11-19 14:17:16 +0100

    wrote update user data

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+
+const filterObj = (obj, allowedFields) => {
+  const newObject = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObject[el] = obj[el];
+  });
+  return newObject;
+};
 
 exports.getAllUsers = catchAsync(async (request, response, next) => {
   const allUsers = await User.find();
@@ -8,6 +17,28 @@ exports.getAllUsers = catchAsync(async (request, response, next) => {
     status: 'success',
     results: allUsers.length,
     data: { ...allUsers },
+  });
+});
+
+exports.updateMe = catchAsync(async (request, response, next) => {
+  if (Object.keys(request.body).length === 0) {
+    const err = 'Please provide a body for the request';
+    return next(new AppError(err, 400));
+  }
+  if (request.body.password || request.body.passwordConfirm) {
+    const err = 'Do not attempt to update passwords here, use /update-password';
+    return next(new AppError(err, 400));
+  }
+  // filter out restricted fields
+  const filteredBody = filterObj(request.body, ['name', 'email']);
+  // since we are not handling sensitive data here its fine to use findbyidandupdate
+  const updatedUser = await User.findByIdAndUpdate(request.user._id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+  response.json({
+    status: 'success',
+    data: updatedUser,
   });
 });
 
