31816b383e4ce181c53e7d3021cebe0cbbeb8ce6
Evgeniy Pimenov <1@evgeniypimenov.com>
2022-01-19 10:49:20 +0100

    pug first steps

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -71,7 +71,10 @@ app.use((req, res, next) => {
 
 // Mounting routers -- YES THIS IS ALSO MIDDLEWARE
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'eugene',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
