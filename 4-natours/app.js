d6362911928efab334f2c0b36eecdac576085d2f
Evgeniy Pimenov <1@evgeniypimenov.com>
2022-01-19 14:45:36 +0100

    app restructuring done

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -15,6 +15,7 @@ const errorController = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 // Initializing application
 const app = express();
@@ -70,25 +71,8 @@ app.use((req, res, next) => {
 });
 
 // Mounting routers -- YES THIS IS ALSO MIDDLEWARE
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'eugene',
-  });
-});
 
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
